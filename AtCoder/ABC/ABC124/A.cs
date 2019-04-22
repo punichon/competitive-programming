007eb8c04bb66a74e6868037f106049f7b703b0b
punichon <46588556+punichon@users.noreply.github.com>
2019-04-23 08:50:15 +0900

    Add C# files

---
+++ AtCoder/ABC/ABC124/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var a = int.Parse(input[0]);
+            var b = int.Parse(input[1]);
+            var ans = Math.Max(2 * a - 1, Math.Max(2 * b - 1, a + b));
+            Console.WriteLine(ans);
+        }
+    }
+}
