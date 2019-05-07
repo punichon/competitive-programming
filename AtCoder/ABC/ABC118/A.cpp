2494de0e30b3c28d8a1c35781175a0c30d11f742
punichon <46588556+punichon@users.noreply.github.com>
2019-05-07 12:53:57 +0900

    Add ABC118 codes

---
+++ AtCoder/ABC/ABC118/A.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<string>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<map>
+#include<climits>
+#include<set>
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long int ll;
+typedef pair<ll,ll> pll;
+int dx[4] = {1,0,0,-1};
+int dy[4] = {0,1,-1,0};
+#define MOD 1000000007
+#define ARRAY_MAX 200005
+
+const int INF = 1e9+7;
+
+int main(){
+
+    int a,b;
+    cin >> a >> b;
+    if(b%a == 0){
+        cout << a+b << endl;
+    }else{
+        cout << b-a << endl;
+    }
+
+
+    return 0;
+}
