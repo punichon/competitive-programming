3d4e0a62501e3e4cbfc51e7bef86325b2135ee3a
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2019-09-02 11:13:05 +0900

    Add ABC0139 codes

---
+++ AtCoder/ABC/ABC101-150/ABC139/C.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cctype>
+#include<utility>
+#include<string>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<map>
+#include<set>
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+	return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+	for (auto& e : t) fill_v(e, v);
+}
+
+
+#define ARRAY_MAX 100005
+const ll INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+int dx[4] = { 1,0,0,-1 };
+int dy[4] = { 0,1,-1,0 };
+
+
+
+
+/******************************************************************************************/
+
+template<typename T>
+T LIS(vector<T>& a) {/*長さに対する最小の最終要素*/
+
+	T siz = a.size();
+	vector<T> dp(a.size() + 10, INF);
+
+	for (T i = 0; i < siz; i++) {
+		*lower_bound(dp.begin(), dp.begin() + siz, a[i]) = a[i];
+	}
+	return lower_bound(dp.begin(), dp.begin() + siz, INF) - dp.begin();
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+
+	int cnt = 0;
+	int maxi = 0;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		if (a[i] >= a[i+1]) {
+			cnt++;
+			//cout << "i = " << i << endl;
+		}
+		else
+		{
+			//cout << "b" << endl;
+			maxi = max(maxi, cnt);
+			cnt = 0;
+		}
+		//cout << "cnt= " << cnt << endl;
+	}
+	maxi = max(maxi, cnt);
+	cout << maxi << endl;
+	
+
+
+	
+
+	return 0;
+}
