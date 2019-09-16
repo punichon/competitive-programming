422558857374dd95caa6bbf5853795094163709b
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2019-09-16 21:27:24 +0900

    Add ABC140 codes

---
+++ AtCoder/ABC/ABC101-150/ABC140/B.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cctype>
+#include<utility>
+#include<string>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<map>
+#include<set>
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+	return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+	for (auto& e : t) fill_v(e, v);
+}
+
+
+#define ARRAY_MAX 100005
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+int dx[4] = { 1,0,0,-1 };
+int dy[4] = { 0,1,-1,0 };
+
+
+
+
+/******************************************************************************************/
+
+int main(){
+
+	int n;
+	cin >> n;
+	vector<int> a(n), b(n), c(n - 1);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		a[i]--;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i];
+	}
+
+	for (int i = 0; i < n-1; i++)
+	{
+		cin >> c[i];
+	}
+
+	int ans = 0;
+	//cout << "a" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		ans += b[a[i]];
+		//cout << "ans = " << ans << endl;
+		if (i < n - 1) {
+			//cout << "i= " << i << endl;
+			if (a[i] + 1 == a[i + 1]) {
+				//cout << "c[i] = " << c[i] << endl;
+				ans += c[a[i]];
+			}
+		}
+		//cout << "ans = " << ans << endl;
+	}
+	
+	cout << ans << endl;
+
+	return 0;
+}
---
+++ AtCoder/ABC/ABC101-150/ABC140/D.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cctype>
+#include<utility>
+#include<string>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<map>
+#include<set>
+#include<climits>
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll, ll> lltpl;
+
+
+
+/******************************************************************************************/
+
+
+const ll MOD = 1e9 + 7;
+
+
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+	return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+	for (auto& e : t) fill_v(e, v);
+}
+
+
+int main() {
+
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+	int ans = 0;
+	for (int i = 0; i < n - 1; i++) {
+		if (s[i] == s[i + 1]) {
+			ans++;
+		}
+	}
+	cout << min(n - 1, ans + k * 2);
+
+	return 0;
+}
