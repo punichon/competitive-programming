465c15d0c1167be4f65a40a46ac780afde081fb3
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-01-16 23:15:36 +0900

    add ABC049 codes

---
+++ AtCoder/ABC/ABC101-150/ABC149/C.cpp
@@ -0,0 +1,176 @@
+#include<algorithm>
+#include<bitset>
+#include<cassert>
+#include<cfloat>
+#include<climits>
+#include<cmath>
+#include<deque>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<vector>
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+template<typename T>
+vector<T> make_v(size_t a) { return vector<T>(a); }
+template<typename T, typename... Ts>
+auto make_v(size_t a, Ts... ts) {
+	return vector<decltype(make_v<T>(ts...))>(a, make_v<T>(ts...));
+}
+template<typename T, typename V>
+typename enable_if<is_class<T>::value == 0>::type
+fill_v(T& t, const V& v) { t = v; }
+template<typename T, typename V>
+typename enable_if<is_class<T>::value != 0>::type
+fill_v(T& t, const V& v) {
+	for (auto& e : t) fill_v(e, v);
+}
+
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T & a, const T & b) {
+	if (a < b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T & a, const T & b) {
+	if (a > b) {
+		a = b;  // aをbで更新
+		return true;
+	}
+	return false;
+}
+
+
+#define ARRAY_MAX 100005
+const int INF = INT32_MAX / 3;
+const ll MOD = 1e9 + 7;
+
+int dx[4] = { 1,0,0,-1 };
+int dy[4] = { 0,1,-1,0 };
+
+
+/******************************************************************************************/
+
+
+struct Combination {
+
+	vector<ll> fact, rfact;
+
+	Combination(ll sz) : fact(sz + 1), rfact(sz + 1) {
+
+		fact[0] = 1;
+		for (ll i = 1; i < fact.size(); i++) {
+			fact[i] = fact[i - 1]%MOD * i % MOD;
+		}
+		//逆元
+		rfact[sz] = inv(fact[sz]);
+		for (ll i = sz - 1; i >= 0; i--) {
+			rfact[i] = rfact[i + 1] %MOD * (i + 1) % MOD;
+		}
+	}
+
+	ll inv(ll x) {
+		return pow(x, MOD - 2);
+	}
+	ll pow(ll x, ll n) {
+		ll ret = 1;
+		while (n > 0) {
+			if (n & 1) (ret *= x) %= MOD;
+			(x *= x) %= MOD;
+			n >>= 1;
+		}
+		return (ret);
+	}
+	ll P(ll n, ll r) {
+		if (r < 0 || n < r) return (0);
+		return (fact[n] * rfact[n - r] % MOD);
+	}
+
+	ll C(ll p, ll q) {
+		if (q < 0 || p < q) return (0LL);
+		return (fact[p]%MOD * rfact[q] % MOD * rfact[p - q] % MOD);
+	}
+
+	ll H(ll n, ll r) {
+		if (n < 0 || r < 0) return (0);
+		return (r == 0 ? 1 : C(n + r - 1, r));
+	}
+};
+
+
+
+/*素数のみの配列を返す*/
+vector<ll> Eratosthenes(ll n, vector<ll>& arr) {
+
+	for (ll i = 0; i < n; i++) {
+		arr[i] = 1;
+	}
+
+	for (ll i = 2; i < sqrt(n); i++) {
+		if (arr[i]) {
+			for (ll j = 0; i * (j + 2) < n; j++) {
+				arr[i * (j + 2)] = 0;
+			}
+		}
+	}
+	vector<ll> prime;
+	for (int i = 2; i < arr.size(); i++) {
+		if (arr[i]) {
+			prime.push_back(i);
+		}
+	}
+	return prime;
+}
+
+
+int main() {
+
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(10);
+
+	
+	ll x;
+	cin >> x;
+	vector<ll> prime(2e5);
+	prime = Eratosthenes(2e5,prime);
+
+	auto itr = lower_bound(prime.begin(), prime.end(), x);
+	cout << *itr << endl;
+
+
+
+	return 0;
+}
+
